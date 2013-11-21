31c4bf81cae7950db790316d39b3cb3af53cf6d2
Peter Organisciak <organis2@illinois.edu>
2013-11-21 16:13:55 -0600

    More stats tracking.

---
+++ workspace/vis-progress.r
@@ -0,0 +1,13 @@
+library(package=ggplot2)
+library(data.table)
+
+stats = read.csv("../logs/stats.csv", sep='\t')
+stats <- data.table(stats)
+stats[,time:=as.POSIXct(time, origin='1970-01-01')]
+
+
+ggplot(stats[name!='total'], aes(x=time, y=count, color=name)) +
+  geom_line() + geom_line(data=stats[name=='total'], size=3) +
+  scale_x_datetime() +
+  theme_minimal()+ theme(legend.position="bottom")
+ggsave("progress.png")
