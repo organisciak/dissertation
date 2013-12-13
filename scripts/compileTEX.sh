2ed1b01eb0ec17b5f61368f3fca312bf02de7f13
Peter Organisciak <organis2@illinois.edu>
2013-12-12 19:54:31 -0600

    Add HTML output: faster than LaTex and thus easier to check during development

--- scripts/compileTEX.sh
+++ scripts/compileTEX.sh
@@ -1,12 +1,19 @@
 if [ '$1' != 'statsonly' ]; then
 	# Convert Chapters to LaTex
-	ls document/*_*.md | parallel pandoc -f markdown -t latex --smart --chapters -o {.}.tex {}
+	ls document/*_*.md | parallel pandoc -f markdown -t latex --biblatex --bibliography=refs.bib --smart --chapters -o {.}.tex {}
+	# Fix underscores in \cite, \autocite, and \textcite commands
+	ls document/*_*.md | parallel "perl -i -p -e 's{cite{.*}}{$& =~ s/_/\\\_/gr}ge' {}"
 
 	# Compile to PDF and view
-	bibtext thesis
+	TEXINPUTS=document/: pdflatex thesis.tex
+	biber thesis
 	TEXINPUTS=document/: pdflatex thesis.tex
 	open thesis.pdf
+
+	cat summary.md document/*_*md | pandoc -f markdown -t html --smart --bibliography=refs.bib --template pandoc-bootstrap-template/template.html --css pandoc-bootstrap-template/template.css -o summary.html
+	open summary.html
 fi;
+
 if [ '$1' != 'latexonly' ]; then
 	# Update stats
 	./scripts/createReport.sh
