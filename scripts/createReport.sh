9a948231dbd170de11605a90eaaedf56776dfd63
Peter Organisciak <organis2@illinois.edu>
2013-12-12 20:07:32 -0600

    Remove stats.log, unnecessary

--- scripts/createReport.sh
+++ scripts/createReport.sh
@@ -1,17 +1,9 @@
-log="logs/stats.log"
 data="logs/stats.csv"
 
-date | tee -a $log
+date 
 
 echo -e "\nWORD COUNT"
-wc -w document/*_*.md | tee -a $log
+wc -w document/*_*.md
 
 # Also save a tab-saparated machine-readable version
 wc -w document/*_*md | perl -pe "s/ +/\t/g" | perl -pe "s/^/`date +%s`/g" >>$data
-
-echo -e "\nSTATS"
-
-echo -e "\nTODOS"
-echo -e "Count\t`grep -rPc "(?<=%) ?(TODO|todo).*$" *.tex`" | tee -a $log
-echo "----" | tee -a $log
-grep -roPn "(?<=%) ?(TODO|todo).*$" *.tex
