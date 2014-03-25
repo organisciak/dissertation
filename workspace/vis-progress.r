30db7f31af66cd979dd2519e6edb2ac72d601e81
Peter Organisciak <organisciak@gmail.com>
2014-03-24 19:20:37 -0500

    Updates to refs and stats tracking

--- workspace/vis-progress.r
+++ workspace/vis-progress.r
@@ -8,13 +8,14 @@ stats[,time:=as.POSIXct(time, origin='1970-01-01')]
 dates = as.numeric(unique(stats$time))
 startdate = dates[1]
 currentdate = tail(dates, 1)
-wps = 12000/(1391212800 - startdate) # Words per second, for slope; 12k words by Feb 1
-intercept = (wps * startdate)
-goal = (wps * currentdate) - intercept # Today's goal
-goal_line = geom_abline(intercept=-intercept, slope=wps)
+#wps = 12000/(1391212800 - startdate) # Words per second, for slope; 12k words by Feb 1
+#intercept = (wps * startdate)
+#goal = (wps * currentdate) - intercept # Today's goal
+#goal_line = geom_abline(intercept=-intercept, slope=wps)
 
 ggplot(stats[name!='total'], aes(x=time, y=count, color=name)) +
-  geom_line() + goal_line + ylim(0, goal) +
+  geom_line() +
+  # goal_line + ylim(0, goal) +
   # Add thicker "TOTAL" line
   geom_line(data=stats[name=='total'], size=3) +
   # Add date vertical lines
