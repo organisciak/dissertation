5696f6276d3a8cbfe58a9aacd31e1571009cb8be
Peter Organisciak <organisciak@gmail.com>
2014-04-03 17:18:03 -0500

    Fixing conflicts

--- scripts/createReport.sh
+++ scripts/createReport.sh
@@ -7,7 +7,7 @@ echo -e "\nWORD COUNT"
 wc -w summary.md document/*_*.md
 
 # Also save a tab-saparated machine-readable version
-wc -w summary.md document/*_*md | perl -pe "s/ +/\t/g" | perl -pe "s/^(\d)/\t\1/g" | perl -pe "s/^/`date +%s`/g" >>$data
+wc -w summary.md document/*_*md | perl -pe "s/ +/\t/g" | perl -pe "s/^/`date +%s`/g" >>$data
 
 # Visualize
 ./scripts/visualizeStats.sh
